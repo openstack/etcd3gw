28daa7970baa8eb3bb01c82a63b5f513cc544057
xuanyandong <xuanyandong@inspur.com>
2021-01-03 11:38:05 +0800

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: I1d1d2ab862725bed2585406e5f215450a42f225e

--- etcd3gw/tests/test_client.py
+++ etcd3gw/tests/test_client.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import mock
+from unittest import mock
 
 from etcd3gw.client import Etcd3Client
 from etcd3gw.exceptions import Etcd3Exception
